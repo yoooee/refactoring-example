48afdfaea4fd09772c9c0f6104e8b1d13d980688
Josef Staroba <jstaroba@centene.com>
2018-10-31 12:45:02 -0500

    Slide Statements Refactor Method - Move variable next to code by which its modified

--- src/statement.ts
+++ src/statement.ts
@@ -4,12 +4,13 @@ interface Performance {
 }
 
 function statement (invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
+
+  let totalAmount = 0;
 
   for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
