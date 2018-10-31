21b0106d5c57902092c39d4296e45ba003e81b5d
Josef Staroba <jstaroba@centene.com>
2018-10-31 11:11:55 -0500

    Move amountFor function inside statement function

--- src/statement.ts
+++ src/statement.ts
@@ -26,30 +26,30 @@ function statement (invoice, plays) {
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
-}
 
-function amountFor(aPerformance: Performance, play) {
-  let result = 0;
+  function amountFor(aPerformance: Performance, play) {
+    let result = 0;
 
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+
+    return result;
   }
-
-  return result;
 }
 
 export { statement };
