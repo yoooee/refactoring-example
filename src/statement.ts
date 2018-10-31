61092cd44af2560061c15911e6ee26ac7fbd89da
Josef Staroba <jstaroba@centene.com>
2018-10-31 13:40:52 -0500

    Add intermediate data object

--- src/statement.ts
+++ src/statement.ts
@@ -4,11 +4,13 @@ interface Performance {
 }
 
 function statement (invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  statementData['customer'] = invoice.customer;
+  return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+function renderPlainText(data, invoice, plays) {
+  let result = `Statement for ${data.customer}\n`;
 
   for (let perf of invoice.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
