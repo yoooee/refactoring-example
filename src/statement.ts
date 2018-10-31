66b1e71eaad5af5b4230b14308e99bf98627a41f
Josef Staroba <jstaroba@centene.com>
2018-10-31 11:50:59 -0500

    Replace Temp with Query Refactor Method - Move currency formatting into it's own function.

--- src/statement.ts
+++ src/statement.ts
@@ -7,9 +7,7 @@ function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US",
-    { style: "currency", currency: "USD",
-      minimumFractionDigits: 2 }).format;
+
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
@@ -21,6 +19,11 @@ function statement (invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
+  function format(aNumber: number) {
+    return new Intl.NumberFormat("en-US",
+      { style: "currency", currency: "USD",
+        minimumFractionDigits: 2 }).format(aNumber);
+  }
   function volumeCreditsFor(aPerformance: Performance) {
     let result = 0;
 
