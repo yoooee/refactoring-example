12709ec861bf71dee8dc9c627ef50d2c6f5bbc34
Josef Staroba <jstaroba@centene.com>
2018-10-31 12:47:43 -0500

    Extract Function Refactor Method - Move volume total Amount logic and loop into separate temporary function

--- src/statement.ts
+++ src/statement.ts
@@ -10,15 +10,21 @@ function statement (invoice, plays) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
 
-  let totalAmount = 0;
-
-  for (let perf of invoice.performances) {
-    totalAmount += amountFor(perf);
-  }
+  let totalAmount = appleSauce();
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
+
+  function appleSauce() {
+    let totalAmount = 0;
+
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf);
+    }
+
+    return totalAmount;
+  }
 
   function usd(aNumber: number) {
     return new Intl.NumberFormat("en-US",
