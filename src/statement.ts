8c59eca287de45ddf53c7a2f0f5f7c6dfc0ed96e
Josef Staroba <jstaroba@centene.com>
2018-11-02 12:31:43 -0500

    Extract Function Refactor Method - Split statement function into two functions.

--- src/statement.ts
+++ src/statement.ts
@@ -4,6 +4,10 @@ interface Performance {
 }
 
 function statement (invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText (invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
