21a0f083dd2f051cbbaf286a9d617ebd263ae8c6
Josef Staroba <jstaroba@centene.com>
2018-11-02 13:29:18 -0500

    Move Function Refactor Method - Move volumeCreditsFor function inside statement function.

--- src/statement.ts
+++ src/statement.ts
@@ -15,6 +15,7 @@ function statement (invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(aPerformance);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -45,6 +46,15 @@ function statement (invoice, plays) {
 
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+
+    return result;
+  }
 }
 
 function renderPlainText (data, plays) {
@@ -72,7 +82,7 @@ function renderPlainText (data, plays) {
     let result = 0;
 
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
 
     return result;
@@ -84,14 +94,6 @@ function renderPlainText (data, plays) {
         minimumFractionDigits: 2 }).format(aNumber/100);
   }
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-  }
 
 
 }
