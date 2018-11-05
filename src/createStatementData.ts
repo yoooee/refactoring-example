7bd9c1f6b934dde8895465f27d5927129025a2c7
Josef Staroba <jstaroba@centene.com>
2018-11-05 08:00:00 -0600

    Inline Function Refactoring Method - Call amount funciton directly.

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -47,7 +47,7 @@ export function createStatementData (invoice, plays) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
